00b778d54701e46c86c2025e077df64e7ea45b2c
Mark Baran <mebaran@gmail.com>
2012-01-23 21:00:52 -0800

    More work on templating engine and interface

--- src/clj_excel/template.clj
+++ src/clj_excel/template.clj
@@ -11,11 +11,20 @@
    (isa? (type stylespec) CellStyle) stylespec
    :else (create-cell-style wb (apply concat stylespec))))
 
-(defn)
+(defn cell-properties
+  "Map of pertinent properties.  Includes row, col, value currently."
+  [cell]
+  {:col (.getColumnIndex cell)
+   :row (.getRowIndex cell)
+   :value (cell-value cell)})
 
 (defn style-cells
   "Templater - takes two functions to load data and style workbooks.
   Takes a hash-map of names to style constructions a collection of
-  criteria, and a function to produce the the data"
-  [style criteria cells]
-  (fn [wb] (doall (map #(.setCellStyle %1 (cell-style wb style)) (filter criteria cells)))))
+  criteria, and a function to produce the the data:
+    -- Map of style names as keywords to style specs
+    -- Map of keywords to filter criteria"
+  [wb stylebook stylesheet cells]
+  (let [style-dict (zipmap (keys stylebook) (map cell-style (vals stylebook)))
+        style-filters (zipmap (keys stylesheet) (style-filter (vals stylesheet)))]
+    ()))
