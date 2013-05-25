0b91cb469899de70bed2774a2273841ce797b883
tok <under.northern.sky@gmail.com>
2013-05-26 00:54:14 +0200

    updating clojure & poi

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-excel "0.0.1"
   :description "Excel bindings for Clojure, based on Apache POI."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.poi/poi "3.8-beta5"]
-                 [org.apache.poi/poi-ooxml "3.8-beta5"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.poi/poi "3.9"]
+                 [org.apache.poi/poi-ooxml "3.9"]])
