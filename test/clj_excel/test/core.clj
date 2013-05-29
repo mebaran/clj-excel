0fd0f6c9ad2e8a50e08e629d83e77d7b8f00238f
tok <under.northern.sky@gmail.com>
2013-05-29 23:42:58 +0200

    another test: roundtrip different value types

--- test/clj_excel/test/core.clj
+++ test/clj_excel/test/core.clj
@@ -2,10 +2,7 @@
   (:use [clj-excel.core])
   (:use [clojure.test])
   (:import [java.io ByteArrayInputStream ByteArrayOutputStream]
-           [org.apache.poi.ss.usermodel WorkbookFactory]))
-
-;; just numbers (doubles in poi); note: rows need not have equal length
-(def trivial-input {"one" [[1.0] [2.0 3.0] [4.0 5.0 6.0]]})
+           [org.apache.poi.ss.usermodel WorkbookFactory DateUtil]))
 
 ;; restore data to nested vecs instead of seqs
 (defn postproc-wb [m]
@@ -26,11 +23,33 @@
     (save wb os)
     (WorkbookFactory/create (ByteArrayInputStream. (.toByteArray os)))))
 
+(defn do-roundtrip [data mode]
+  (-> (wb-from-data data mode) (save-load-cycle) (lazy-workbook) (postproc-wb)))
+
 ;; compare the data to the original
 (defn valid-workbook-roundtrip? [data mode]
-  (-> (wb-from-data data mode) (save-load-cycle) (lazy-workbook) (postproc-wb)
-      (= data)))
+  (= data (do-roundtrip data mode)))
+
+
+;; just numbers (doubles in poi); note: rows need not have equal length
+(def trivial-input {"one" [[1.0] [2.0 3.0] [4.0 5.0 6.0]]})
 
 (deftest roundtrip-trivial
   (is (valid-workbook-roundtrip? trivial-input :xssf))
   (is (valid-workbook-roundtrip? trivial-input :hssf)))
+
+
+;; FIXME: Dates need hand holding :-(
+(defn fix-date [e] [[(DateUtil/getJavaDate (ffirst e))]])
+(defn fixed-roundtrip? [data mode]
+  (-> (do-roundtrip data mode) (update-in ["four"] fix-date)
+      (= data)))
+
+(def now (java.util.Date.))
+;; multiple sheets with different cell types
+(def many-sheets {"one"   [[1.0]]   "two"  [["hello"]]
+                  "three" [[false]] "four" [[now]]})
+
+(deftest roundtrip-many
+  (is (fixed-roundtrip? many-sheets :xssf))
+  (is (fixed-roundtrip? many-sheets :hssf)))
