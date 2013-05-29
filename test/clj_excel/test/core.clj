133d792fb82810da523fc1a2d0080c81fe4800c6
tok <under.northern.sky@gmail.com>
2013-05-29 22:45:46 +0200

    first test

--- test/clj_excel/test/core.clj
+++ test/clj_excel/test/core.clj
@@ -1,6 +1,36 @@
 (ns clj-excel.test.core
   (:use [clj-excel.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:import [java.io ByteArrayInputStream ByteArrayOutputStream]
+           [org.apache.poi.ss.usermodel WorkbookFactory]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+;; just numbers (doubles in poi); note: rows need not have equal length
+(def trivial-input {"one" [[1.0] [2.0 3.0] [4.0 5.0 6.0]]})
+
+;; restore data to nested vecs instead of seqs
+(defn postproc-wb [m]
+  (->> (for [[k v] m]
+         [k (vec (map vec v))])
+       (into {})))
+
+;; build a workbook
+(defn wb-from-data [data & opts]
+  (let [as-set (set opts)
+        wb     (cond (contains? as-set :hssf) (workbook-hssf)
+                     :else                    (workbook-xssf))]
+    (build-workbook wb data)))
+
+;; save & reload
+(defn save-load-cycle [wb]
+  (let [os (ByteArrayOutputStream.)]
+    (save wb os)
+    (WorkbookFactory/create (ByteArrayInputStream. (.toByteArray os)))))
+
+;; compare the data to the original
+(defn valid-workbook-roundtrip? [data mode]
+  (-> (wb-from-data data mode) (save-load-cycle) (lazy-workbook) (postproc-wb)
+      (= data)))
+
+(deftest roundtrip-trivial
+  (is (valid-workbook-roundtrip? trivial-input :xssf))
+  (is (valid-workbook-roundtrip? trivial-input :hssf)))
