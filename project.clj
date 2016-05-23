c6ed435c39039601abbef1af19b4fb96c6bd31c5
Robin Gower <robin.git@infonomics.ltd.uk>
2016-05-23 15:33:55 +0200

    Update dependencies: clojure and namespace tools

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject com.outpace/clj-excel "0.0.9-SNAPSHOT"
   :description "Excel bindings for Clojure, based on Apache POI."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.poi/poi "3.9"]
                  [org.apache.poi/poi-ooxml "3.9"]]
   :profiles {:dev {:source-paths ["dev"]
                    :resource-paths ["test-resources"]
                    :plugins [[lein-cloverage "1.0.2"]]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :global-vars {*print-length* 20}}
              :test {:resource-paths ["test-resources"]}}
   :global-vars {*warn-on-reflection* true}
